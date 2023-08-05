0d76a6222234cf42b48c5fd628e8206615a2eb9d
Stanislav Derpoliuk <derpoliuk@gmail.com>
2023-08-05 19:32:03 -0300

    Initial Commit

---
+++ Lego Star Wars Character Classifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lego Star Wars Character Classifier
+//
+//  Created by Stanislav Derpoliuk on 2023-08-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lego Star Wars Character Classifier/Lego_Star_Wars_Character_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lego_Star_Wars_Character_ClassifierApp.swift
+//  Lego Star Wars Character Classifier
+//
+//  Created by Stanislav Derpoliuk on 2023-08-05.
+//
+
+import SwiftUI
+
+@main
+struct Lego_Star_Wars_Character_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
